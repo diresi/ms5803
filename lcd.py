249ff6904af2cb961122e342eed4b895ae6571ae
Christoph Rissner <resi@rissner.net>
2013-04-07 17:23:44 +0200

    adapted to TLS8204 (now that I know my LCD uses it :))

--- lcd.py
+++ lcd.py
@@ -1,19 +1,26 @@
-"""LCD playground (Nokia 3310 via SPI)
+"""LCD playground (Nokia 3310 via SPI driven by a TLS8204 as available from
+https://www.olimex.com/Products/Modules/LCD/MOD-LCD3310/).
+
+I don't use extended commands other than level 1 nor OTP.
+Note: This implementation uses TLS8204 but is almost compatible to PCD8544,
+except start_line_ctrl(), which is not available on the latter.
 """
 import time
 import RPIO
 import spi
-from static import ASCII
+from static import ASCII, AWESOME
 
 LCD_RESET = 24
 DC = 25
 
-LCD_START_LINE_ADDR = 66 -2
+LCD_START_LINE_ADDR = 66 - 2
+LCD_WIDTH = 84
+LCD_HEIGHT = 48
+LCD_LINES = LCD_HEIGHT / 8
 
 def gpio(port, level):
     f = RPIO.gpio_function(port)
     if f:
-        print "setup output"
         RPIO.setup(port, RPIO.OUT, level)
     RPIO.forceoutput(port, level)
 
@@ -21,12 +28,12 @@ class Nokia3310(object):
     def __init__(self, dc=DC, lcd_reset=LCD_RESET):
         self.dc = dc
         self.lcd_reset = lcd_reset
+        self.ext = 0x00
 
     def reset(self):
         gpio(self.lcd_reset, RPIO.LOW)
-        time.sleep(1)
         gpio(self.lcd_reset, RPIO.HIGH)
-        time.sleep(1)
+        self.ext = 0x00
 
     def send(self, dc, data):
         gpio(self.dc, dc)
@@ -42,63 +49,103 @@ class Nokia3310(object):
     def command(self, data):
         self.send(RPIO.LOW, data)
 
+    def function_set(self, ext = 0x00, powerdown = False):
+        cmd = 0x20 | (0x03 & ext)
+        if powerdown:
+            cmd |= 0x04
+        self.ext = ext
+        self.command(cmd)
+
+    # ext = 0x00
+    def display_ctrl(self, mode = "normal"):
+        assert self.ext == 0x00
+        # uses bits 3 and 0 to mask 4 commands
+        ctrl = {"normal" : 0x04,
+                "inverse" : 0x05,
+                "all" : 0x01,
+                "none" : 0x00,
+               }
+        assert mode in ("normal", "inverse", "all", "none")
+        cmd = 0x08 | ctrl[mode]
+        self.command(cmd)
+
+    def set_x(self, x):
+        assert self.ext == 0x00
+        self.command(0x80 | (0x7F & x))
+
+    def set_y(self, y):
+        assert self.ext == 0x00
+        self.command(0x40 | (0x07 & y))
+
+    def goto(self, x, y):
+        self.set_x(x)
+        self.set_y(y)
+
+    def vlcd_range_ctrl(self, high = False):
+        assert self.ext == 0x00
+        cmd = 0x04 | (0x01 if high else 0x00)
+        self.command(cmd)
+
+    # ext = 0x01
+    def display_config(self, reverse = False):
+        # defines the bit order mapping for display ram (top down or bottom up)
+        assert self.ext == 0x01
+        self.command(0x08 | (0x04 if reverse else 0x00))
+
+    def bias_ctrl(self, mode):
+        assert self.ext == 0x01
+        self.command(0x10 | (0x07 & mode))
+
+    def start_line_ctrl(self, line):
+        assert self.ext == 0x01
+        self.command(0x04 | (0x01 & (line >> 6)))
+        self.command(0x40 | (0x3F & line))
+
+    def evr_ctrl(self, vop):
+        assert self.ext == 0x01
+        self.command(0x80 | (0x7F & vop))
+
     def setup(self):
-        self.command(0x21)   # LCD Extended Commands
-        self.command(0xFF)   # Set LCD Vop (Contras)
+        self.reset()
 
-        # line mapping mode? this sets up 6 lines with 8px each. without these
-        # 2 commands the lcd uses 7 lines where the first has 5px and the last 3px
-        self.command(0x04 | int(not(not(LCD_START_LINE_ADDR & (1 << 6)))))   #Set Temp S6 for start line
-        self.command(0x40 | (LCD_START_LINE_ADDR & ((1<<6)-1)))              #Set Temp S[5:0] for start line
+        self.function_set(0x00)
+        self.vlcd_range_ctrl(False)
 
-        self.command(0x06)   # LCD temperature coefficent to 2
-        #self.command(0x13)  # LCD bias mode 1:48.
-        self.command(0x12)   # LCD bias mode 1:68.
-        self.command(0x20)   # LCD Standard Commands, Horizontal addressing mode
-        #self.command(0x22)  # LCD Standard Commands, Vertical addressing mode
+        self.function_set(0x01)
+        self.start_line_ctrl(LCD_START_LINE_ADDR) # 6 lines with 8px each
+        self.bias_ctrl(4)     # empirically determined
+        self.evr_ctrl(80)     # empirically determined
+
+        self.function_set(0x00)
 
         self.clear()
-        self.mode()
-        #self.command(0x08)   # LCD blank (overlay, doesn't clear screen contents)
-        #self.command(0x09)   # LCD filled (overlay, doesn't clear screen contents)
-
-    def mode(self, normal = True):
-        if normal:
-            self.command(0x0C)   # LCD in normal mode
-        else:
-            self.command(0x0D)   # LCD in inverse mode
+        self.display_ctrl("normal")
 
     def fill(self, overlay = False, pattern = 0xFF):
         if overlay:
-            self.command(0x09)
+            self.display_ctrl("all")
         else:
-            for l in range(7):
+            for l in range(LCD_LINES):
                 self.goto(0, l)
-                self.data((pattern,) * 84)
+                self.data((pattern,) * LCD_WIDTH)
             self.goto(0, 0)
 
     def clear(self, overlay = False):
         if overlay:
-            self.command(0x08)
+            self.display_ctrl("none")
         else:
             self.fill(pattern = 0x00)
 
     def test(self):
-        for y in range(6):
-            self.command(0x80)
-            self.command(0x40 | y)
-            for x in range(84):
-                self.data(0xa5)
-
-    def goto(self, x, y):
-        assert x < 84
-        assert y < 7
-        self.command(0x80 | x)
-        self.command(0x40 | y)
+        self.fill(pattern = 0xA5)
 
     def bitmap(self, bmp):
-        self.goto(0, 0)
-        self.data(bmp)
+        # TLS8204 uses 102 columns and 68 lines, we can't just send the bitmap
+        # as-is.
+        for l in range(LCD_LINES):
+            self.goto(0, l)
+            offset = l * LCD_WIDTH
+            self.data(bmp[offset:offset + LCD_WIDTH])
 
     def character(self, c):
         self.data(0x00)
@@ -109,18 +156,32 @@ class Nokia3310(object):
         for x in txt:
             self.character(x)
 
+    def watch(self):
+        self.goto(LCD_WIDTH - 8 * 7, LCD_LINES - 1)
+        self.text(time.strftime("%H:%M:%S"))
+
 def setup():
     spi.openSPI(mode = 0, speed = 1000000, delay = 0)
     lcd = Nokia3310()
-    lcd.reset()
     lcd.setup()
     return lcd
 
 if __name__ == "__main__":
     lcd = setup()
-    lcd.test()
-    time.sleep(2)
 
-    lcd.clear()
-    lcd.goto(0, 0)
-    lcd.text("resi ruleZ!")
+    while True:
+        lcd.test()
+        time.sleep(1)
+
+        lcd.clear()
+        lcd.goto(0, 2)
+        lcd.text("resi ruleZ!")
+        time.sleep(1)
+
+        lcd.bitmap(AWESOME)
+        time.sleep(1)
+        break
+
+    while True:
+        lcd.watch()
+        time.sleep(.5)
